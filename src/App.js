dc825ac4cd1a9727fa338118980d9d048dcfbd8f
rishabhbhatia57 <[rishabhbhatia57@gmail.com]>
2021-11-22 02:39:54 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import CompetetionDetails from "./pages/CompetetionDetails";
 import PanellistsInfo from "./pages/PanellistsInfo";
 import Platform from "./pages/Platform";
@@ -10,22 +11,50 @@ import Spnsors from "./pages/Spnsors";
 import Timeline from "./pages/Timeline";
 function App() {
   return (
-    
     <Router>
       <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> CompetetionDetails </Link></li>
-            <li><Link to={'/PanellistsInfo'} className="nav-link">PanellistsInfo</Link></li>
-            <li><Link to={'/Platform'} className="nav-link">Platform</Link></li>
-            <li><Link to={'/Prizes'} className="nav-link">Prizes</Link></li>
-            <li><Link to={'/RegistrationInfo'} className="nav-link">RegistrationInfo</Link></li>
-            <li><Link to={'/Spnsors'} className="nav-link">Spnsors</Link></li>
-            <li><Link to={'/Timeline'} className="nav-link">Timeline</Link></li>
+            <li>
+              <Link to={"/"} className="nav-link">
+                {" "}
+                CompetetionDetails{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/PanellistsInfo"} className="nav-link">
+                PanellistsInfo
+              </Link>
+            </li>
+            <li>
+              <Link to={"/Platform"} className="nav-link">
+                Platform
+              </Link>
+            </li>
+            <li>
+              <Link to={"/Prizes"} className="nav-link">
+                Prizes
+              </Link>
+            </li>
+            <li>
+              <Link to={"/RegistrationInfo"} className="nav-link">
+                RegistrationInfo
+              </Link>
+            </li>
+            <li>
+              <Link to={"/Spnsors"} className="nav-link">
+                Spnsors
+              </Link>
+            </li>
+            <li>
+              <Link to={"/Timeline"} className="nav-link">
+                Timeline
+              </Link>
+            </li>
           </ul>
-          </nav>
-          
-      {/* <div>
+        </nav>
+
+        {/* <div>
         <Link to="/">CompetetionDetails</Link>
       </div>
       <div>
@@ -46,21 +75,22 @@ function App() {
       <div>
         <Link to="/Timeline">Timeline</Link>
       </div> */}
-   
+
         <Routes>
-          <Route exact path="/" element={<CompetetionDetails/>}/>
-          <Route exact path="/PanellistsInfo" element={<PanellistsInfo/>}/>
-          <Route exact path="/Platform" element={<Platform/>}/>
-          <Route exact path="/Prizes" element={<Prizes/>}/>
-          <Route exact path="/RegistrationInfo" element={<RegistrationInfo/>}/>
-          <Route exact path="/Spnsors" element={<Spnsors/>}/>
-          <Route exact path="/Timeline" element={<Timeline/>}/>    
+          <Route exact path="/" element={<CompetetionDetails />} />
+          <Route exact path="/PanellistsInfo" element={<PanellistsInfo />} />
+          <Route exact path="/Platform" element={<Platform />} />
+          <Route exact path="/Prizes" element={<Prizes />} />
+          <Route
+            exact
+            path="/RegistrationInfo"
+            element={<RegistrationInfo />}
+          />
+          <Route exact path="/Spnsors" element={<Spnsors />} />
+          <Route exact path="/Timeline" element={<Timeline />} />
         </Routes>
-    
 
-  
-
-      {/* <Routes>
+        {/* <Routes>
         
         <Route exact path="/">
           <CompetetionDetails />
